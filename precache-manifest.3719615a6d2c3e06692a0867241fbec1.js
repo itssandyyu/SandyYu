f3c026f3a15ae2a4112119179cc386e98970a78f
Sandy Yu <sandy@Sandys-iMac.local>
2024-07-23 19:08:07 -0700

    Updates

---
+++ precache-manifest.3719615a6d2c3e06692a0867241fbec1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3920a12401d9471fb25ff071247bc24b",
+    "url": "/SandyYu/index.html"
+  },
+  {
+    "revision": "34dff27caee069e60033",
+    "url": "/SandyYu/static/css/2.35f98baf.chunk.css"
+  },
+  {
+    "revision": "ffb190b6e5470abeae59",
+    "url": "/SandyYu/static/css/main.14819343.chunk.css"
+  },
+  {
+    "revision": "34dff27caee069e60033",
+    "url": "/SandyYu/static/js/2.218e247a.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/SandyYu/static/js/2.218e247a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ffb190b6e5470abeae59",
+    "url": "/SandyYu/static/js/main.0d1d2fda.chunk.js"
+  },
+  {
+    "revision": "1fc78406cb87df0ccf79",
+    "url": "/SandyYu/static/js/runtime-main.f5a4c984.js"
+  },
+  {
+    "revision": "b3e455412babe2b902911e0ad1aff374",
+    "url": "/SandyYu/static/media/MMA.b3e45541.png"
+  },
+  {
+    "revision": "fc58e7839eba94210b125721e1380e09",
+    "url": "/SandyYu/static/media/Sandy-Yu.fc58e783.jpg"
+  },
+  {
+    "revision": "2fe3a22f9c9eba0dc1d28f67ccb80e7f",
+    "url": "/SandyYu/static/media/Webgen.2fe3a22f.png"
+  },
+  {
+    "revision": "dad660926109a92590a6f95fc5de52d8",
+    "url": "/SandyYu/static/media/c4me.dad66092.png"
+  },
+  {
+    "revision": "97a974d272a7887aebb0f6b81d08fd0d",
+    "url": "/SandyYu/static/media/csg.97a974d2.png"
+  },
+  {
+    "revision": "f86c10d1430712de80e6010bb2a5ff29",
+    "url": "/SandyYu/static/media/e-comm.f86c10d1.png"
+  },
+  {
+    "revision": "78294cb06f3e737344c90312e54e0144",
+    "url": "/SandyYu/static/media/twitter.78294cb0.jpg"
+  }
+]);
